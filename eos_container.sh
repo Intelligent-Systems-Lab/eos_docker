e6a4e5feaba7a366bd07789389158bb0aefecb42
tony <t>
2019-10-23 17:57:39 +0800

    init

---
+++ eos_container.sh
@@ -0,0 +1,26 @@
+apt-get update && \
+apt-get install -y software-properties-common && \
+apt-get install -y    \
+    autotools-dev     \
+    automake          \
+    cmake             \
+    curl              \
+    fuse              \
+    git               \
+    wget              \
+    zip               \
+    build-essential   \
+    gcc               \
+    pkg-config        \
+    csh               \
+    tcsh              \
+    byacc             \
+    flex              \
+    liblzma-dev       \
+    lib1g-dev         \
+    libblas-dev       \
+    liblapack-dev     \
+    gfortran          \
+    libfreetype6-dev  \
+    libpng-dev        \
+        libjpeg8-dev
