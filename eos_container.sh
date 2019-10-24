0ecf7dd28e0ce7a1195acef19a17f0a570940cba
tony <t>
2019-10-24 09:43:20 +0800

    add reme

--- eos_container.sh
+++ eos_container.sh
@@ -1,12 +1,12 @@
-apt-get update && \
-apt-get install -y software-properties-common
+apt-get update && apt-get update &&\
+apt-get install -y software-properties-common &&\
 apt-get install -y autotools-dev automake cmake curl git wget zip build-essential gcc pkg-config net-tools
 
 mkdir /home/user
 sh -c "echo \"export HOME=/home/user\">> ~/.bashrc"
 source ~/.bashrc
 
-echo "Because we change $HOME, we should souece  in /root/.bashrc"
+echo "Because we change HOME, we should souece  in /root/.bashrc"
 # source /root/.bashrc
 sh -c "echo \"alias sb='source /root/.bashrc'\" >> /root/.bashrc"
 source /root/.bashrc
