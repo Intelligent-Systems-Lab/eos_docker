53ff4a0f0f261514a22b574c92d09a65d940bdd6
tony <t>
2019-10-24 22:59:38 +0800

    eos install

--- eos_container.sh
+++ eos_container.sh
@@ -10,3 +10,15 @@ echo "Because we change HOME, we should souece  in /root/.bashrc"
 # source /root/.bashrc
 sh -c "echo \"alias sb='source /root/.bashrc'\" >> /root/.bashrc"
 source /root/.bashrc
+
+# install eos
+wget https://github.com/EOSIO/eos/releases/download/v1.7.0/eosio_1.7.0-1-ubuntu-16.04_amd64.deb
+apt -y install ./eosio_1.7.0-1-ubuntu-16.04_amd64.deb
+wget https://github.com/EOSIO/eosio.cdt/releases/download/v1.6.1/eosio.cdt_1.6.1-1_amd64.deb
+apt -y install ./eosio.cdt_1.6.1-1_amd64.deb
+mkdir contracts
+cd contracts
+git clone --recursive https://github.com/eosio/eosio.cdt --branch v1.6.1 --single-branch
+cd eosio.cdt
+./build.sh
+./install.sh
